ddb658da26e98e13eb8621851eac4b9c82737fde
ishani1024 <ishanibasu76@gmail.com>
2026-08-17 20:41:52 +0530

    p3.cpp

---
+++ .vscode/p3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, originalNum, remainder, reversedNum = 0;
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    originalNum = num;
+    while (num > 0) {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+    if (originalNum < 0) {
+        cout << originalNum << " is not a palindrome." << endl;
+    } else if (originalNum == reversedNum) {
+        cout << originalNum << " is a palindrome." << endl;
+    } else {
+        cout << originalNum << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
