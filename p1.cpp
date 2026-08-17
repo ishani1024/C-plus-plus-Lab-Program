3d30acde3feaf73d063a5f5f8b0b134ce04244c7
ishani1024 <ishanibasu76@gmail.com>
2026-08-17 20:24:29 +0530

    https://github.com/ishani1024/C-plus-plus-Lab-Program.git

---
+++ p1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "Enter three integers: ";
+    cin >> a >> b >> c;
+
+    if (a >= b && a >= c)
+        cout << "Largest = " << a;
+    else if (b >= a && b >= c)
+        cout << "Largest = " << b;
+    else
+        cout << "Largest = " << c;
+
+    return 0;
+}
