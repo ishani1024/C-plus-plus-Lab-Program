e5f3b02e68cc26d380ae737db623ba80d477c883
ishani1024 <ishanibasu76@gmail.com>
2026-08-17 21:02:42 +0530

    p3.cpp

--- p3.cpp
+++ p3.cpp
@@ -8,11 +8,15 @@ int main() {
     cin >> num;
 
     originalNum = num;
+
+    // Reverse the number
     while (num > 0) {
         remainder = num % 10;
         reversedNum = reversedNum * 10 + remainder;
         num /= 10;
     }
+
+    // Check if original number and reversed number are equal
     if (originalNum < 0) {
         cout << originalNum << " is not a palindrome." << endl;
     } else if (originalNum == reversedNum) {
