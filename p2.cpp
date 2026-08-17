87080d0fcb555ea3d545a46d59296cda7a73b017
ishani1024 <ishanibasu76@gmail.com>
2026-08-17 20:31:20 +0530

    p2.cpp

---
+++ p2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, fact = 1;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    cout << "Factorial = " << fact;
+
+    return 0;
+}
